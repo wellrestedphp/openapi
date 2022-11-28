a9a16bfdea0c0c80d3c8b1603c94f96da3d991cb
PJ Dietz <pjdietz@gmail.com>
2022-11-28 09:45:59 -0500

    Refactor ReflectionResolver

--- src/ReflectionResolver.php
+++ src/ReflectionResolver.php
@@ -12,6 +12,14 @@ use ReflectionNamedType;
 use WellRESTed\Server;
 use WellRESTed\ServerReferenceTrait;
 
+/**
+ * Locates ReflectionClasses for handlers.
+ *
+ * WellRESTed dispatches handlers and middlewware from instances, class names,
+ * DI services, factory functions, arrays, etc. ReflectionResolver accepts
+ * these as inputs and returns an array of ReflectionClass instances for
+ * classes associated with the handler and middleware.
+ */
 class ReflectionResolver
 {
     use ServerReferenceTrait;
@@ -26,25 +34,46 @@ class ReflectionResolver
     {
         $reflections = [];
 
-        if (is_string($handler)) {
-            if (class_exists($handler)) {
-                $reflections[] = $this->fromClass($handler);
-            } else {
-                $reflections[] = $this->fromService($handler);
-            }
+        if (is_array($handler)) {
+            $reflections = [...$reflections, ...$this->fromArray($handler)];
+        } elseif (is_string($handler)) {
+            $reflections[] = $this->fromString($handler);
         } elseif ($handler instanceof Closure) {
             $reflections[] = $this->fromClosure($handler);
-        } elseif (is_array($handler)) {
-            foreach ($handler as $item) {
-                $reflections = array_merge($reflections, $this->getReflections($item));
-            }
         } elseif (is_object($handler)) {
             $reflections[] = $this->fromClass($handler);
         }
 
-        return array_filter($reflections);
+        return $this->filter($reflections);
     }
 
+    /**
+     * A route may have an array of middleware and handlers associated. Locate
+     * each middleware and handler and return an array.
+     *
+     * @return ReflectionClass[]
+     */
+    private function fromArray(array $handler): array
+    {
+        $reflections = [];
+
+        foreach ($handler as $item) {
+            $reflections = [...$reflections, ...$this->getReflections($item)];
+        }
+
+        return $reflections;
+    }
+
+    private function fromString(string $handler): ?ReflectionClass
+    {
+        return class_exists($handler)
+            ? $this->fromClass($handler)
+            : $this->fromService($handler);
+    }
+
+    /**
+     * Create the ReflectionClass for a handler from an instance or class name.
+     */
     private function fromClass(string|object $handler): ?ReflectionClass
     {
         $reflection = null;
@@ -53,30 +82,49 @@ class ReflectionResolver
             $reflection = new ReflectionClass($handler);
         }
 
-        if ($reflection && $reflection->implementsInterface(ResponseInterface::class)) {
-            return null;
-        }
         return $reflection;
     }
 
+    /**
+     * Locate the handler by service name from the DI container (if present).
+     */
     private function fromService(string $serviceName): ?ReflectionClass
     {
+        $reflection = null;
         $container = $this->getServer()->getContainer();
+
         if ($container && $container->has($serviceName)) {
             $service = $container->get($serviceName);
-            return $this->fromClass($service);
+            $reflection = $this->fromClass($service);
         }
-        return null;
+
+        return $reflection;
     }
 
+    /**
+     * Handlers may be registered with factory functions. When the function has
+     * a return type, use that to determing the handler's class.
+     */
     private function fromClosure(Closure $handler): ?ReflectionClass
     {
+        $reflection = null;
         $reflectionFn = new ReflectionFunction($handler);
         $returnType = $reflectionFn->getReturnType();
+
         if ($returnType instanceof ReflectionNamedType) {
             $name = $returnType->getName();
-            return $this->fromClass($name);
+            $reflection = $this->fromClass($name);
         }
-        return null;
+
+        return $reflection;
+    }
+
+    private function filter(array $reflections): array
+    {
+        $fn = function (?ReflectionClass $reflection): bool {
+            return $reflection && !$reflection->implementsInterface(ResponseInterface::class);
+        };
+
+        return array_filter($reflections, $fn);
     }
 }
